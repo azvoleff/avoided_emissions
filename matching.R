0e65baa2bfc755369bff2adaead64ad7367032d2
Alex Zvoleff <azvoleff@gmail.com>
2020-05-07 15:41:36 -0400

    Add more progress messages.

--- matching.R
+++ matching.R
@@ -95,7 +95,7 @@ match_ae <- function(d, f) {
         # glm
         dists <- match_on(f, data=d)
     }
-    dists <- caliper(dists, 2)
+    #dists <- caliper(dists, 2)
     # If the controls are too far from the treatments (due to the caliper) 
     # then the matching may fail. Can test for this by seeing if subdim 
     # runs successfully
@@ -159,7 +159,7 @@ ae <- foreach(row_num=1:nrow(sites),
     Data_Year <- p$Data_Year
     if (file.exists(paste0('Output/m_', CI_ID, '_', Data_Year, '.RData'))) {
         print(paste('Skipping', CI_ID, Data_Year))
-        next
+        return(NULL)
     }
     # sink(output_log, append=TRUE, type='output')
     # sink(message_log, append=TRUE, type="message")
@@ -172,13 +172,22 @@ ae <- foreach(row_num=1:nrow(sites),
     if ((length(inter) == 0) || !inter) {
         # Some areas won't overlap the GADM at all (marine sites for example). 
         # Return no results for these areas.
+        print(paste0(row_num, ": polygon doesn't overlap GADM at all"))
         return(NULL)
     } else {
         d_crop <- gdal_crop(d, regions[inter, ])
     }
 
-    print(paste0(row_num, ': Rasterizing and masking'))
-    p_rast <- rasterize(p, d_crop, background=0)
+    print(paste0(row_num, ': Adding treatment indicator layer'))
+    d_crop$treatment <- 0
+    d_crop$treatment[is.na(d_crop$region)] <- NA
+    d_crop$treatment <- mask(d_crop$treatment, p)
+
+    if (cellStats(d_crop$treatment, 'sum') == 0) {
+        print(paste0(row_num, ': No treatment values - perhaps a marine site?'))
+        return(NULL)
+    }
+
     # Note that the below will drop any portions of a site that don't fall 
     # within a region in GADM- this means that marine areas will be dropped. 
     # Given this calculation is for avoided emissions, likely not a problem... 
@@ -187,9 +196,6 @@ ae <- foreach(row_num=1:nrow(sites),
     #
     # TODO: Need to ensure we also mask out other areas where CI has ongoing 
     # interventions.
-    treat_or_control <- mask(p_rast, d_crop$region)
-    names(treat_or_control) <- 'treatment'
-    d_crop <- stack(treat_or_control, d_crop)
 
     # Project all items to cylindrical equal area
     # d_crop <- projectRaster(d_crop, crs=CRS('+proj=cea'), method='ngb')
@@ -199,68 +205,81 @@ ae <- foreach(row_num=1:nrow(sites),
     
     treatment_vals <- extract(d_crop, p, df=TRUE)
     treatment_vals <- treatment_vals[names(treatment_vals) != 'ID']
-    print(paste0(row_num, ': ', nrow(treatment_vals), ' treatment cells'))
-
+    n_treatment_cells_total <- nrow(treatment_vals)
+    print(paste0(row_num, ': ', n_treatment_cells_total, ' total treatment cells'))
     ###
     # Sample the treatment cells if there are more than MAX_TREATMENT
-    if (nrow(treatment_vals) > MAX_TREATMENT) {
-        print(paste0(row_num, ': sampling treatment cells'))
+    if (n_treatment_cells_total <= MAX_TREATMENT) {
+        n_treatment_cells_sample <- n_treatment_cells_total
+    } else {
+        n_treatment_cells_sample <- MAX_TREATMENT
         treatment_vals <- sample_n(treatment_vals, MAX_TREATMENT)
+        print(paste0(row_num, ': sampled ', nrow(treatment_vals), ' treatment cells'))
     }
     ###
     # Sample the control cells if there are more than 100*MAX_TREATMENT
     #
     # Note the below factor is set slightly higher than the one used with 
     # sampling to ensure that there are enough cells to sample from
-    if (ncell(d_crop) < (120 * nrow(treatment_vals))) {
+    if (ncell(d_crop) < (120 * n_treatment_cells_sample)) {
         print(paste0(row_num, ': using all possible control points'))
         control_vals <- data.frame(getValues(d_crop))
-        control_vals <- dplyr::filter(control_vals, treatment == 0)
     } else {
-        print(paste0(row_num, ': sampling control points'))
-        control_vals <- as.data.frame(sampleRegular(d_crop, 100 * nrow(treatment_vals), useGDAL=TRUE))
-        control_vals <- dplyr::filter(control_vals, treatment == 0)
+        control_vals <- as.data.frame(sampleRegular(d_crop,
+                                                    100 * n_treatment_cells_sample, 
+                                                    useGDAL=TRUE))
+        print(paste0(row_num, ': sampled ', nrow(control_vals), ' control cells'))
     }
+    # Ensure no treatment cells were picked up within the control points
+    control_vals <- dplyr::filter(control_vals, treatment == 0)
     vals <- rbind(treatment_vals, control_vals)
+    # Remove any points with missing data for treatment indicator
     vals <- vals[!is.na(vals$treatment), ]
+    print(paste0(row_num, ': ', nrow(vals), ' total cells in matching'))
     vals$treatment <- as.logical(vals$treatment)
     vals$region <- as.factor(vals$region)
     vals$biome <- as.factor(vals$biome)
     vals$ecoregion <- as.factor(vals$ecoregion)
     vals$pa <- as.factor(vals$pa)
 
-    print(paste0(row_num, ': Matching'))
-    m <- match_ae(vals, f)
-
-    print(paste0(row_num, ': Formatting output'))
-    if (is.null(m)) {
-        print(paste0(row_num, ': no matches'))
+    if (nrow(filter(vals, treatment))) {
+        print(paste0(row_num, ': No treatment values remaining after filtering'))
+        return(NULL)
     } else {
-        m <- dplyr::select(m, c(get_names(f),
-                         'fc_2000',
-                         'fc_2001',
-                         'fc_2002',
-                         'fc_2003',
-                         'fc_2004',
-                         'fc_2005',
-                         'fc_2006',
-                         'fc_2007',
-                         'fc_2008',
-                         'fc_2009',
-                         'fc_2010',
-                         'fc_2011',
-                         'fc_2012',
-                         'fc_2013',
-                         'fc_2014',
-                         'fc_2015',
-                         'fc_2016',
-                         'fc_2017',
-                         'fc_2018'))
-        m$CI_ID <- CI_ID
-        m$Data_Year <- Data_Year
-        m <- m %>% dplyr::select(CI_ID, everything())
-        print(paste0(row_num, ': saving output'))
+        print(paste0(row_num, ': Matching'))
+        m <- match_ae(vals, f)
+
+        print(paste0(row_num, ': Formatting output'))
+        if (is.null(m)) {
+            print(paste0(row_num, ': no matches'))
+        } else {
+            m <- dplyr::select(m, c(get_names(f),
+                             'fc_2000',
+                             'fc_2001',
+                             'fc_2002',
+                             'fc_2003',
+                             'fc_2004',
+                             'fc_2005',
+                             'fc_2006',
+                             'fc_2007',
+                             'fc_2008',
+                             'fc_2009',
+                             'fc_2010',
+                             'fc_2011',
+                             'fc_2012',
+                             'fc_2013',
+                             'fc_2014',
+                             'fc_2015',
+                             'fc_2016',
+                             'fc_2017',
+                             'fc_2018'))
+            m$CI_ID <- CI_ID
+            m$Data_Year <- Data_Year
+            m <- m %>% dplyr::select(CI_ID, everything())
+            print(paste0(row_num, ': saving output'))
+        m$sampled_fraction <- n_treatment_cells_sample / n_treatment_cells_total
+        save(m, file=paste0('Output/m_', CI_ID, '_', Data_Year, '.RData'))
+        }
     }
-    save(m, file=paste0('Output/m_', CI_ID, '_', Data_Year, '.RData'))
-    return(NULL)
+    return(m)
 }
