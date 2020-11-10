62ff0e0bdeda3c6d743c0edf04d7a801d93cbc9c
Alex Zvoleff <azvoleff@conservation.org>
2020-11-10 18:08:00 -0500

    Cleanup sampling.

--- 2_2_matching.R
+++ 2_2_matching.R
@@ -1,8 +1,14 @@
 library(tidyverse)
 library(optmatch)
 library(foreach)
+library(lubridate)
+library(biglm)
+library(tictoc)
+
+options("optmatch_max_problem_size"=Inf)
 
 MAX_TREATMENT <- 2000
+CONTROL_MULTIPLIER <- 50
     
 # Function to allow rbinding dataframes with foreach even when some dataframes 
 # may not have any rows
@@ -27,50 +33,21 @@ get_names <- function(f) {
 }
 
 match_ae <- function(d, f) {
-    # Filter out sites without at least one treatment unit or without at
-    # least one control unit
-    d <- d %>%
-        filter(complete.cases(.)) %>%
-        mutate(n_treatment=sum(treatment),
-               n_control=sum(!treatment)) %>%
-        filter(n_treatment >= 1, n_control >= 1)
-
-    # Note custom combine to handle iterations that don't return any value
-    d_treatment <- filter(d, treatment)
-    # Filter out climates and land covers that don't appear in the CI
-    # sample, and drop these levels from the factors
-    d <- filter(d,
-        region %in% unique(d_treatment$region),
-        ecoregion %in% unique(d_treatment$ecoregion),
-        pa %in% unique(d_treatment$pa))
-    d$region <- droplevels(d$region)
-    d$ecoregion <- droplevels(d$ecoregion)
-    d$pa <- droplevels(d$pa)
-    # Can't stratify by land cover or climate if they only have one level
-    if (nlevels(d$region) >= 2) {
-        f <- update(f, ~ . + strata(region))
-    } else {
-        f <- update(f, ~ . - region)
-    }
-    if (nlevels(d$ecoregion) >= 2) {
-        f <- update(f, ~ . + strata(ecoregion))
-    } else {
-        f <- update(f, ~ . - ecoregion)
-    }
-    if (nlevels(d$pa) >= 2) {
-        f <- update(f, ~ . + strata(pa))
-    } else {
-        f <- update(f, ~ . - pa)
-    }
-    if (nrow(d_treatment) > 2) {
-        model <- glm(f, data=d)
+    if (sum(d$treatment) > 30) {
+        if (nlevels(d$group) >= 2) {
+            f <- update(f, ~ . + strata(group))
+        } else {
+            f <- update(f, ~ . - strata(group))
+        }
+        model <- glm(f, data=d, family=binomial())
         dists <- match_on(model, data=d)
     } else {
         # Use Mahalanobis distance if there aren't enough points to run a
         # glm
         dists <- match_on(f, data=d)
     }
-    # If the controls are too far from the treatments (due to the caliper) then 
+
+    # If the controls are too far from the treatments (due to a caliper) then 
     # the matching may fail. Can test for this by seeing if subdim runs 
     # successfully
     subdim_works <- tryCatch(is.data.frame(subdim(dists)),
@@ -93,57 +70,62 @@ match_ae <- function(d, f) {
 ###############################################################################
 ###  Load sites and covariates
 
-f <- readRDS('Output/formula.RDS')
 treatment_key <- readRDS('Output/treatment_cell_key.RDS')
+readRDS('sites.RDS') %>%
+    select(-geometry) %>%
+    as_tibble() -> sites
+
+# Filter to include only values of group that appear in the treatment pixels, 
+# and to not include values that appear only in the treatment pixels
+filter_groups <- function(vals) {
+    vals$group <- interaction(vals$region, vals$ecoregion, vals$pa)
+    vals <- filter(vals, group %in% unique(filter(vals, treatment)$group))
+    treatment_groups <- unique(filter(vals, treatment)$group)
+    control_groups <- unique(filter(vals, !treatment)$group)
+    vals <- filter(vals, group %in% treatment_groups[treatment_groups %in% control_groups])
+    # Filter out values of group that appear ONLY in the treatment pixels
+    vals$group <- droplevels(vals$group)
+    return(vals)
+}
 
 ###############################################################################
 ###  Run matching
-set.seed(31)
-this_CI_ID <- unique(treatment_key$CI_ID)[1]
 
-year <- 2018
-this_CID_ID <- unique(treatment_key$CI_ID)[12]
+set.seed(31)
 # TODO: Need to account for the data year
-ae <- foreach(this_CI_ID in unique(treatment_key$CI_ID),
+year <- 2018
+
+###############################################################################
+# TODO: Remove this, for testing only
+ids <- unique(treatment_key$CI_ID)[c(12, 13, 30)]
+treatment_key <- treatment_key %>% filter(CI_ID %in% ids)
+###############################################################################
+
+ae <- foreach(this_CI_ID=unique(treatment_key$CI_ID),
               .combine=foreach_rbind, .inorder=FALSE) %do% {
 
+    tic()
+    site <- filter(sites,
+                   CI_ID == this_CI_ID,
+                   Data_Year == year)
     if (file.exists(paste0('Output/m_', this_CI_ID, '_', year, '.RDS'))) {
-        print(paste('Skipping', this_CI_ID, year, '. Already processed.'))
+        print(paste0('Skipping ', this_CI_ID, ' for year ', year, '. Already processed.'))
         return(NULL)
+    } else {
+        print(paste0('Processing ', this_CI_ID, ' for year ', year, '.'))
     }
 
-    # TODO: Need to account for the data year
     treatment_cell_IDs <- filter(treatment_key,
                                  CI_ID == this_CI_ID,
+                                 !is.na(region),
                                  Data_Year == year)
-    n_treatment_cells_total <- nrow(treatment_cell_IDs)
-    print(paste0(this_CI_ID, ': ', n_treatment_cells_total, ' total treatment cells'))
-    ###
-    # Sample the treatment cells if there are more than MAX_TREATMENT
-    if (n_treatment_cells_total <= MAX_TREATMENT) {
-        n_treatment_cells_sample <- n_treatment_cells_total
-    } else {
-        n_treatment_cells_sample <- MAX_TREATMENT
-        treatment_cell_IDs <- sample_n(treatment_cell_IDs, MAX_TREATMENT)
-        print(paste0(this_CI_ID, ': sampled ', nrow(treatment_cell_IDs), ' treatment cells'))
-    }
 
+    n_treatment_cells_total <- nrow(treatment_cell_IDs)
     vals <- foreach(this_region = unique(treatment_cell_IDs$region),
                     .combine=rbind) %do% {
         v <- readRDS(paste0('Output/treatments_and_controls_', this_region, 
                             '.RDS'))
         filter(v, region == this_region)
-    }
-
-    # Sample the control cells if there are more than 100*MAX_TREATMENT
-    #
-    # Note the below factor is set slightly higher than the one used with 
-    # sampling to ensure that there are enough cells to sample from
-    if (nrow(vals) < (120 * n_treatment_cells_sample)) {
-        print(paste0(this_CI_ID, ': using all possible control points'))
-    } else {
-        vals <- sample_n(vals, 100 * n_treatment_cells_sample)
-        print(paste0(this_CI_ID, ': sampled ', nrow(vals), ' control cells'))
     }
 
     # TODO: need to knockout other CI sites from control sample by setting 
@@ -152,19 +134,62 @@ ae <- foreach(this_CI_ID in unique(treatment_key$CI_ID),
             treatment_cell_IDs %>%
                 select(cell, Data_Year) %>%
                 mutate(treatment=TRUE)
-            ) -> vals
+            , by='cell') -> vals
+    vals$treatment <- as.logical(vals$treatment)
     vals$treatment[is.na(vals$treatment)] <- FALSE
+    vals$Data_Year <- year
+
+    # Eliminate any groups that are only in the control pixels, or only in the 
+    # treatment pixels
+    vals <- filter_groups(vals)
+
+    sample_sizes <- vals %>%
+        count(treatment, group)
+    # Sample the treatment cells if there are more than MAX_TREATMENT pixels, 
+    # and the control cells if there are more than CONTROL_MULTIPLIER * 
+    # MAX_TREATMENT pixels
+    bind_rows(
+            filter(vals, treatment)  %>%
+                group_by(group) %>%
+                sample_n(min(MAX_TREATMENT, n())),
+            filter(vals, !treatment)  %>%
+                group_by(this_group=group) %>%
+                sample_n(min(CONTROL_MULTIPLIER * filter(sample_sizes,
+                                                         treatment == TRUE,
+                                                         group == this_group[1])$n,
+                             n()))
+            ) %>% 
+        ungroup() -> vals
+    # Refilter in case any groups were lost due to the sampling
+    vals <- filter_groups(vals)
 
     # Project all items to cylindrical equal area
     # d_crop <- projectRaster(d_crop, crs=CRS('+proj=cea'), method='ngb')
+    
+    f <- readRDS('Output/formula.RDS')
+    f <- update(f, ~ . + group)
 
-    # Remove any points with missing data for treatment indicator
-    vals <- vals[!is.na(vals$treatment), ]
-    print(paste0(this_CI_ID, ': ', nrow(vals), ' total cells in matching'))
-    vals$treatment <- as.logical(vals$treatment)
-    vals$region <- as.factor(vals$region)
-    vals$ecoregion <- as.factor(vals$ecoregion)
-    vals$pa <- as.factor(vals$pa)
+    # For sites that were established in or after 2005, match on the five years 
+    # of deforestation data preceding the year of establishment. For sites 
+    # estab prior to 2005, don't match on defor rate
+    estab_year <- year(site$CI_Start_Date_clean)
+    if (estab_year >= 2005) {
+        init <- vals[, grepl(paste0('fc_', substr(estab_year - 5, 3, 4)), names(vals))]
+        final <- vals[,grepl(paste0('fc_', substr(estab_year, 3, 4)), names(vals))]
+        defor_pre_intervention <- ((final - init) / init) * 100
+        # Correct for division by zero in places that had no forest cover in 
+        # year 0
+        defor_pre_intervention[init == 0] <- 0
+        names(defor_pre_intervention) <- 'defor_pre_intervention'
+        vals <- cbind(vals, defor_pre_intervention)
+        f <- update(f, ~ . + defor_pre_intervention)
+    }
+    #vals <- vals %>% select(-starts_with('fc_'), -starts_with('fcc_'))
+    
+    sample_sizes <- vals %>%
+        count(treatment, group)
+    print(paste0(this_CI_ID, ': ', paste(filter(sample_sizes, treatment)$n, collapse=', '), ' treatment pixels'))
+    print(paste0(this_CI_ID, ': ', paste(filter(sample_sizes, !treatment)$n, collapse=', '), ' control pixels'))
 
     if (nrow(filter(vals, treatment)) == 0) {
         print(paste0(this_CI_ID, ': No treatment values remaining after filtering'))
@@ -172,45 +197,18 @@ ae <- foreach(this_CI_ID in unique(treatment_key$CI_ID),
     } else {
         print(paste0(this_CI_ID, ': Matching'))
         m <- match_ae(vals, f)
-
         print(paste0(this_CI_ID, ': Formatting output'))
         if (is.null(m)) {
             print(paste0(this_CI_ID, ': no matches'))
         } else {
-            m <- dplyr::select(m, c(get_names(f),
-                             'region',
-                             'ecoregion',
-                             'pa',
-                             'fc_00',
-                             'fc_01',
-                             'fc_02',
-                             'fc_03',
-                             'fc_04',
-                             'fc_05',
-                             'fc_06',
-                             'fc_07',
-                             'fc_08',
-                             'fc_09',
-                             'fc_10',
-                             'fc_11',
-                             'fc_12',
-                             'fc_13',
-                             'fc_14',
-                             'fc_15',
-                             'fc_16',
-                             'fc_17',
-                             'fc_18',
-                             'fc_19'))
-            m$CI_ID <- CI_ID
-            m$Data_Year <- Data_Year
+            m$CI_ID <- this_CI_ID
+            m$Data_Year <- year
             m <- m %>% dplyr::select(CI_ID, everything())
             print(paste0(this_CI_ID, ': saving output'))
         m$sampled_fraction <- n_treatment_cells_sample / n_treatment_cells_total
-        saveRDS(m, paste0('Output/m_', CI_ID, '_', Data_Year, '.RDS')
+        saveRDS(m, paste0('Output/m_', this_CI_ID, '_', year, '.RDS'))
         }
     }
-
+    toc()
     return(m)
 }
-
-d_crop <- as.data.table(d)
